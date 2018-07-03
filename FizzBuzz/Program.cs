6f1c97f00771b23dfac68562c10bca3e4babc783
MikhiavSmith <36665285+MikhiavSmith@users.noreply.github.com>
2018-07-03 19:37:06 -0400

    Importing project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string f = "555";
+			Console.WriteLine(f);
+			Console.ReadLine();
+		}
+		public static string FizzBuzz(int num)
+		{
+			if (num % 15 == 0)
+			{
+				return "FizzBuzz";
+			}
+			else if (num % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else if (num % 5 == 0)
+			{
+				return "Buzz";
+			}
+			else
+			{
+				return "False";
+			}
+		}
+	}
+}
